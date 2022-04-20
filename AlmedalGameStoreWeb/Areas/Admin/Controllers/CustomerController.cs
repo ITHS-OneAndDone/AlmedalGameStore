3a7194e21616694d9a74682fd45cbb15486ffc6e
JonasOlsson90 <69483396+Kim-Jong-Jonas@users.noreply.github.com>
2022-04-20 11:15:45 +0200

    Started on customer view for admin.

---
+++ AlmedalGameStoreWeb/Areas/Admin/Controllers/CustomerController.cs
@@ -0,0 +1,29 @@
+﻿using AlmedalGameStore.DataAccess.GenericRepository.IGenericRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlmedalGameStoreWeb.Areas.Admin.Controllers;
+public class CustomerController : Controller
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    private readonly IWebHostEnvironment _hostEnviorment;
+
+    public CustomerController(IUnitOfWork unitOfWork, IWebHostEnvironment hostEnvironment)
+    {
+        _unitOfWork = unitOfWork;
+        _hostEnviorment = hostEnvironment;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        var userList = _unitOfWork.ApplicationUser.GetAll(includeProperties: "Role");
+
+        return Json(new { data = userList });
+    }
+}
